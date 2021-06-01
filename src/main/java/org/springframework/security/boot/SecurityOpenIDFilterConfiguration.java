2b5ecbb76731854d4623312edf35b205425757f5
wandl <hiwepy@163.com>
2021-06-01 15:22:56 +0800

    session优化

--- src/main/java/org/springframework/security/boot/SecurityOpenIDFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityOpenIDFilterConfiguration.java
@@ -1,9 +1,12 @@
 package org.springframework.security.boot;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openid4java.consumer.ConsumerException;
 import org.openid4java.consumer.ConsumerManager;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.biz.web.servlet.i18n.LocaleContextFilter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -18,14 +21,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationFailureHandler;
-import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationSuccessHandler;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.boot.biz.authentication.AuthenticationListener;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
 import org.springframework.security.boot.biz.property.SecuritySessionMgtProperties;
 import org.springframework.security.boot.openid.userdetails.OpenIDAuthcUserDetailsService;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.openid.AxFetchListFactory;
 import org.springframework.security.openid.NullAxFetchListFactory;
@@ -34,9 +38,13 @@ import org.springframework.security.openid.OpenIDAttribute;
 import org.springframework.security.openid.OpenIDAuthenticationFilter;
 import org.springframework.security.openid.OpenIDAuthenticationProvider;
 import org.springframework.security.openid.OpenIDConsumer;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.savedrequest.RequestCache;
 
 @Configuration
 @AutoConfigureBefore(name = { 
@@ -85,40 +93,66 @@ public class SecurityOpenIDFilterConfiguration implements EnvironmentAware {
 	@Configuration
 	@EnableConfigurationProperties({ SecurityOpenIDProperties.class, SecurityBizProperties.class })
 	@Order(SecurityProperties.DEFAULT_FILTER_ORDER + 7)
-	static class OpenIDWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	static class OpenIDWebSecurityConfigurerAdapter extends WebSecurityBizConfigurerAdapter {
 
 		private SecurityBizProperties bizProperties;
+
+		private final SecurityOpenIDAuthcProperties authcProperties;
 		
 		private OpenIDConsumer openIDConsumer;
 		private AuthenticationManager authenticationManager; 
-		private RememberMeServices rememberMeServices;
-		private SessionAuthenticationStrategy sessionStrategy;
-		
+
+
+	    private final AuthenticationEntryPoint authenticationEntryPoint;
+	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+	    private final AuthenticationFailureHandler authenticationFailureHandler;
+	    
+    	private final LocaleContextFilter localeContextFilter;
+    	
 		private final ConsumerManager consumerManager;
 		private final OpenIDAttribute attribute;
 	    private final OpenIDAuthenticationFilter openIDAuthenticationFilter;
 	    private final OpenIDAuthenticationProvider openIDAuthenticationProvider;
-	    private final PostRequestAuthenticationSuccessHandler authenticationSuccessHandler;
-	    private final PostRequestAuthenticationFailureHandler authenticationFailureHandler;
 		private final OpenIDAuthcUserDetailsService openIDAuthcUserDetailsService;
 		private final OpenIDConsumer consumer;
-		private final SecurityOpenIDAuthcProperties authcProperties;
+		
+    	private final RequestCache requestCache;
+    	private final RememberMeServices rememberMeServices;
 		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
 	
 		public OpenIDWebSecurityConfigurerAdapter(
 
+				SecurityBizProperties bizProperties,
 				SecuritySessionMgtProperties sessionMgtProperties,
 				SecurityOpenIDAuthcProperties authcProperties,
-				
+
+   				ObjectProvider<LocaleContextFilter> localeContextProvider,
+   				ObjectProvider<AuthenticationProvider> authenticationProvider,
+   				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
+   				ObjectProvider<AuthenticationListener> authenticationListenerProvider,
+   				ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
+   				ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
+   				ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider, 
+   				
 				ObjectProvider<OpenIDAttribute> attributeProvider,
 				ObjectProvider<OpenIDAuthenticationFilter> openIDAuthenticationFilterProvider,
 				ObjectProvider<OpenIDAuthenticationProvider> openIDAuthenticationProvider,
 				ObjectProvider<OpenIDAuthcUserDetailsService> openIDAuthcUserDetailsService, 
 				ObjectProvider<OpenIDConsumer> consumerProvider,
 				ObjectProvider<ConsumerManager> consumerManagerProvider,
-				@Qualifier("jwtAuthenticationSuccessHandler") ObjectProvider<PostRequestAuthenticationSuccessHandler> authenticationSuccessHandler,
-   				@Qualifier("jwtAuthenticationFailureHandler") ObjectProvider<PostRequestAuthenticationFailureHandler> authenticationFailureHandler,
+				ObjectProvider<RememberMeServices> rememberMeServicesProvider,
 				ObjectProvider<SessionAuthenticationStrategy> sessionAuthenticationStrategyProvider) {
+			
+			super(bizProperties, authcProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()),
+					authenticationManagerProvider.getIfAvailable());
+			
+			this.localeContextFilter = localeContextProvider.getIfAvailable();
+   			List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
+   			this.authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+   			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+   			this.requestCache = super.requestCache();
+   			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
 			this.attribute = attributeProvider.getIfAvailable();
 			this.authcProperties = authcProperties;
 			this.openIDAuthenticationFilter = openIDAuthenticationFilterProvider.getIfAvailable();
@@ -126,8 +160,6 @@ public class SecurityOpenIDFilterConfiguration implements EnvironmentAware {
 			this.openIDAuthcUserDetailsService = openIDAuthcUserDetailsService.getIfAvailable();
 			this.consumer = consumerProvider.getIfAvailable();
 			this.consumerManager = consumerManagerProvider.getIfAvailable();
-			this.authenticationSuccessHandler = authenticationSuccessHandler.getIfAvailable();
-   			this.authenticationFailureHandler = authenticationFailureHandler.getIfAvailable();
 			this.sessionAuthenticationStrategy = sessionAuthenticationStrategyProvider.getIfAvailable();
 		}
 
@@ -139,7 +171,7 @@ public class SecurityOpenIDFilterConfiguration implements EnvironmentAware {
 			 */
 			PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
 			
-			map.from(authcProperties.getSessionMgt().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
+			map.from(getSessionMgtProperties().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
 			
 			map.from(authenticationManager).to(authenticationFilter::setAuthenticationManager);
 			map.from(authenticationSuccessHandler).to(authenticationFilter::setAuthenticationSuccessHandler);
@@ -165,7 +197,15 @@ public class SecurityOpenIDFilterConfiguration implements EnvironmentAware {
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
 			
-			http.openidLogin()
+			http.requestCache()
+	        	.requestCache(requestCache)
+	        	.and()
+	        	.exceptionHandling()
+	        	.authenticationEntryPoint(authenticationEntryPoint)
+	        	.and()
+	        	.httpBasic()
+	        	.disable()
+	        	.openidLogin()
 				.attributeExchange(authcProperties.getIdentifierPattern())
 				.attribute(attribute)
 				.and()
@@ -177,11 +217,9 @@ public class SecurityOpenIDFilterConfiguration implements EnvironmentAware {
 				.failureUrl(authcProperties.getFailureUrl())
 				.loginProcessingUrl(authcProperties.getLoginUrl())
 				.successHandler(this.authenticationSuccessHandler)
-				.and()
-				.sessionManagement()
-				.sessionAuthenticationStrategy(sessionAuthenticationStrategy)
-	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	            .and()
+	        	.antMatcher(authcProperties.getPathPattern())
+   	        	.addFilterBefore(localeContextFilter, UsernamePasswordAuthenticationFilter.class)
 	            .addFilterBefore(openIDAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 			
 		}
